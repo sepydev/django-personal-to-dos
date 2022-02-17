7286f98444288a886986341a17e341e8dcce8f18
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-17 13:14:02 +0330

    Add owner permission to CoreValueViewSet
    *Set the user as the owner when creating core values
    *Users can only get their core values

--- core_values/views/core_value_views.py
+++ core_values/views/core_value_views.py
@@ -1,18 +1,15 @@
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.viewsets import ModelViewSet
-
+from core.views import ModelViewSet, OwnerListModelViewSetMixin, SetOwnerModelViewListMixin
+from users.permisions import IsOwnerPermission
 from ..models import CoreValue as CoreValueModel
 from ..serializers import CoreValueSummarySerializer, CoreValueDetailSerializer
 
 
-class CoreValueViewSet(ModelViewSet):
+class CoreValueViewSet(OwnerListModelViewSetMixin, SetOwnerModelViewListMixin, ModelViewSet):
     queryset = CoreValueModel.objects.all()
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsOwnerPermission,)
     serializer_class = CoreValueSummarySerializer
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return CoreValueDetailSerializer
         return CoreValueSummarySerializer
-
-
