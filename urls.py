945a1db150c9e5481eea1c4417f2e9188e342997
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-20 20:11:12 +0330

    Create API for get images

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import CoreValueViewSet, GoalViewSet
+from .views import CoreValueViewSet, GoalViewSet, MedialView
 
 router = routers.DefaultRouter()
 router.register('core-value', CoreValueViewSet)
@@ -9,5 +9,6 @@ router.register('goal', GoalViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('media/images/<str:file>', MedialView.as_view(), name='images'),
 
 ]
--- views.py
+++ views.py
@@ -1,2 +1,17 @@
 from .core_values.views import CoreValueViewSet  # noqa
 from .goals.views import GoalViewSet  # noqa
+
+from django.conf import settings
+import os
+from django.http.response import FileResponse
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class MedialView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, file):
+        full_path = os.path.join(settings.BASE_DIR, settings.MEDIA_ROOT, "images/", file)
+        response = FileResponse(open(full_path, 'rb'))
+        return response
