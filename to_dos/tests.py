31272b09e245549972445f9ad1a7b569c5762fab
Mohammad Rezazadeh <mrprocs@gmail.com>
2022-02-27 22:36:46 +0330

    To-do list retrieval and testing it (monthly tasks)

--- to_dos/tests.py
+++ to_dos/tests.py
@@ -4,13 +4,14 @@ import json
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from helpers.date_time import add_months
 from personal_to_dos.core_values.tests.test_api import create_core_value
 from personal_to_dos.goals.tests import create_goal
 from personal_to_dos.tasks.tests import create_task
 from users.tests.test_api import create_user_verify_and_login
 
 
-class DailyTasksTodoAPI(APITestCase):
+class TodoAPI(APITestCase):
     def setUp(self) -> None:
         token = create_user_verify_and_login(self.client)
         core_value_response = create_core_value(self.client, token)
@@ -20,39 +21,7 @@ class DailyTasksTodoAPI(APITestCase):
         self.token = token
         self.goal_id = goal_id
 
-    def test_daily_unlimited_task(self):
-        """
-        Testing get to-do api for a task with this condition
-        *daily
-        *unlimited
-        *repeat every day
-        *not completed
-
-        the result have to have this task
-        """
-
-        create_task(self.client, self.token, self.goal_id,
-                    title="Daily-unlimited",
-                    start_date_time=datetime.date.today() - datetime.timedelta(days=20),
-                    repeat_type="Day",
-                    repeat_period=1,
-                    end_type="Never",
-                    completely_done=False
-                    )
-
-        response = self.client.get(
-            '/personal-to-dos/to-do/',
-            {
-                'date': datetime.date.today()
-            },
-            headers={
-                'Authorization': self.token
-            }
-        )
-
-        self.assertContains(response, "Daily-unlimited", status_code=status.HTTP_200_OK)
-
-    def test_daily_expired_task(self):
+    def test_expired_task(self):
         """
         Testing get to-do api for a task with this condition
         *daily
@@ -108,12 +77,86 @@ class DailyTasksTodoAPI(APITestCase):
         self.assertContains(response, "Daily-expired-today", status_code=status.HTTP_200_OK)
         self.assertNotContains(response, "Daily-expired-last-week", status_code=status.HTTP_200_OK)
 
+    def test_done_task(self):
+        create_task(self.client, self.token, self.goal_id,
+                    title="task completely done",
+                    start_date_time=datetime.date.today() - datetime.timedelta(days=20),
+                    repeat_type="Day",
+                    repeat_period=1,
+                    end_type="On Specific date",
+                    end_date=datetime.date.today() + datetime.timedelta(days=101),
+                    completely_done=True
+                    )
+
+        response = self.client.get(
+            '/personal-to-dos/to-do/',
+            {
+                'date': datetime.date.today()
+            },
+            headers={
+                'Authorization': self.token
+            }
+        )
+
+        self.assertNotContains(response, "task completely done", status_code=status.HTTP_200_OK)
+
+
+class DailyTasksTodoAPI(APITestCase):
+    def setUp(self) -> None:
+        token = create_user_verify_and_login(self.client)
+        core_value_response = create_core_value(self.client, token)
+        core_value_id = json.loads(core_value_response.content)['pk']
+        goal_response = create_goal(self.client, token, core_value_id)
+        goal_id = json.loads(goal_response.content)['pk']
+        self.token = token
+        self.goal_id = goal_id
+
+    def test_daily_unlimited_task(self):
+        """
+        Testing get to-do api for a task with this condition
+        *daily
+        *unlimited
+        *repeat every day
+        *not completed
+
+        the result have to have this task
+        """
+
+        create_task(self.client, self.token, self.goal_id,
+                    title="Daily-unlimited",
+                    start_date_time=datetime.date.today() - datetime.timedelta(days=20),
+                    repeat_type="Day",
+                    repeat_period=1,
+                    end_type="Never",
+                    completely_done=False
+                    )
+
+        response = self.client.get(
+            '/personal-to-dos/to-do/',
+            {
+                'date': datetime.date.today()
+            },
+            headers={
+                'Authorization': self.token
+            }
+        )
+
+        self.assertContains(response, "Daily-unlimited", status_code=status.HTTP_200_OK)
+
     def test_daily_task_repeat_period(self):
         create_task(self.client, self.token, self.goal_id,
                     title="Daily-not-contains-repeat_period=2",
                     start_date_time=datetime.date.today() - datetime.timedelta(days=1),
                     repeat_type="Day",
                     repeat_period=2,
+                    end_type="Never",
+                    completely_done=False
+                    )
+        create_task(self.client, self.token, self.goal_id,
+                    title="Daily-not-contains-repeat_period=3",
+                    start_date_time=datetime.date.today() - datetime.timedelta(days=1),
+                    repeat_type="Day",
+                    repeat_period=3,
                     end_type="Never",
                     completely_done=False
                     )
@@ -129,3 +172,68 @@ class DailyTasksTodoAPI(APITestCase):
         )
 
         self.assertNotContains(response, "Daily-not-contains-repeat_period=2", status_code=status.HTTP_200_OK)
+        self.assertNotContains(response, "Daily-not-contains-repeat_period=3", status_code=status.HTTP_200_OK)
+
+
+class MonthlyTasksTodoAPI(APITestCase):
+    def setUp(self) -> None:
+        token = create_user_verify_and_login(self.client)
+        core_value_response = create_core_value(self.client, token)
+        core_value_id = json.loads(core_value_response.content)['pk']
+        goal_response = create_goal(self.client, token, core_value_id)
+        goal_id = json.loads(goal_response.content)['pk']
+        self.token = token
+        self.goal_id = goal_id
+
+    def test_monthly_unlimited_task(self):
+        create_task(self.client, self.token, self.goal_id,
+                    title="Monthly-unlimited",
+                    start_date_time=add_months(datetime.date.today(), -1),
+                    repeat_type="Month",
+                    repeat_period=1,
+                    end_type="Never",
+                    completely_done=False
+                    )
+
+        response = self.client.get(
+            '/personal-to-dos/to-do/',
+            {
+                'date': datetime.date.today()
+            },
+            headers={
+                'Authorization': self.token
+            }
+        )
+
+        self.assertContains(response, "Monthly-unlimited", status_code=status.HTTP_200_OK)
+
+    def test_monthly_task_repeat_period(self):
+        create_task(self.client, self.token, self.goal_id,
+                    title="Monthly-not-contains-repeat_period=2",
+                    start_date_time=add_months(datetime.date.today(), -1),
+                    repeat_type="Month",
+                    repeat_period=2,
+                    end_type="Never",
+                    completely_done=False
+                    )
+        create_task(self.client, self.token, self.goal_id,
+                    title="Monthly-not-contains-repeat_period=3",
+                    start_date_time=add_months(datetime.date.today(), -1),
+                    repeat_type="Month",
+                    repeat_period=3,
+                    end_type="Never",
+                    completely_done=False
+                    )
+
+        response = self.client.get(
+            '/personal-to-dos/to-do/',
+            {
+                'date': datetime.date.today()
+            },
+            headers={
+                'Authorization': self.token
+            }
+        )
+
+        self.assertNotContains(response, "Monthly-not-contains-repeat_period=2", status_code=status.HTTP_200_OK)
+        self.assertNotContains(response, "Monthly-not-contains-repeat_period=3", status_code=status.HTTP_200_OK)
--- to_dos/views.py
+++ to_dos/views.py
@@ -16,21 +16,37 @@ class TodoAPIView(ListModelMixin, GenericViewSet):
     serializer_class = ToDoSummarySerializer
 
     def get_queryset(self):
-        _date = self.request.query_params['date']
+        _date = datetime.datetime.strptime(self.request.query_params['date'], '%Y-%m-%d')
         _queryset = self.queryset
         _queryset = _queryset.annotate(
-            diff_date=(ExpressionWrapper(datetime.datetime.now() - F('start_date_time'),
+            diff_date=(ExpressionWrapper(_date - F('start_date_time'),
                                          output_field=IntegerField()) / 86400000000),
-            daily_repeat_period_condition=(F('diff_date') % Cast('repeat_period', IntegerField()))
-        )
-        _daily = (
-                Q(repeat_type=RepeatTypeChoices.DAY) &
-                (
-                        Q(end_type=EndTypeChoices.NEVER) |
-                        (Q(end_type=EndTypeChoices.DATE, end_date__gte=_date))
-                ) &
-                Q(daily_repeat_period_condition=0)
-        )
-        _queryset = _queryset.filter(_daily)
+            diff_month=(_date.month - F('start_date_time__month')),
+            daily_repeat_period_condition=(F('diff_date') % Cast('repeat_period', IntegerField())),
+            monthly_repeat_period_condition=(F('diff_month') % Cast('repeat_period', IntegerField())),
 
+        )
+        _expire_conditions = (
+                Q(end_type=EndTypeChoices.NEVER) |
+                (Q(end_type=EndTypeChoices.DATE, end_date__gte=_date))
+        )
+        _not_done_condition = Q(completely_done=False)
+
+        _daily_conditions = Q(
+            repeat_type=RepeatTypeChoices.DAY,
+            daily_repeat_period_condition=0,
+        )
+        _monthly_conditions = Q(
+            repeat_type=RepeatTypeChoices.MONTH,
+            start_date_time__day=_date.day,
+            monthly_repeat_period_condition=0,
+        )
+        _queryset = _queryset.filter(
+            _expire_conditions &
+            _not_done_condition &
+            (
+                    _daily_conditions |
+                    _monthly_conditions
+            )
+        )
         return _queryset
